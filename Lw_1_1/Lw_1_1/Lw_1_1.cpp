2d6919cbd031942ac9726ee16710648c3810e456
Ernest Aleksandrov <ernest_aleksandrov@mail.ru>
2019-02-25 17:46:23 +0300

    Первая лаба: \c
    /
    exit

---
+++ Lw_1_1/Lw_1_1/Lw_1_1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "boost/timer.hpp"
+
+using namespace std;
+
+int main()
+{
+	vector<size_t> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	boost::timer t;
+
+	t.restart();
+	sort(v.begin(), v.end());
+	do
+	{
+	} while (next_permutation(v.begin(), v.end()));
+	double duration = t.elapsed();
+
+	cout << duration << endl;
+	return 0;
+}
---
+++ Lw_1_1/Lw_1_1/pch.h
@@ -0,0 +1,14 @@
+﻿#include <SDKDDKVer.h>
+#include "stdio.h"
+#include <tchar.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+
+#ifndef PCH_H
+#define PCH_H
+
+
+#endif //PCH_H
